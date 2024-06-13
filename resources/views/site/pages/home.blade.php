cceb145f60e99661273abedecd5e9fb7717c6dc1
Arlindo <arlindoarnaldocassuende@gmail.com>
2024-06-13 01:18:27 +0100

    Mostrando posts recomendados em ordem aleatoria

--- resources/views/site/pages/home.blade.php
+++ resources/views/site/pages/home.blade.php
@@ -110,84 +110,38 @@
           </div>
         </div>
       </div>
+      @if ($recommended_posts)
       <div class="col-lg-12 col-md-6">
         <div class="widget">
           <h2 class="section-title mb-3">Recomendados</h2>
           <div class="widget-body">
             <div class="widget-list">
-              <article class="card mb-4">
-                <div class="card-image">
-                  <div class="post-info"> <span class="text-uppercase">1 minutes read</span>
-                  </div>
-                  <img loading="lazy" decoding="async" src="/site-assets/images/post/post-9.jpg" alt="Post Thumbnail" class="w-100">
-                </div>
-                <div class="card-body px-0 pb-1">
-                  <h3><a class="post-title post-title-sm"
-                      href="article.html">Portugal and France Now
-                      Allow Unvaccinated Tourists</a></h3>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor …</p>
-                  <div class="content"> <a class="read-more-btn" href="article.html">Read Full Article</a>
-                  </div>
-                </div>
-              </article>
-              <a class="media align-items-center" href="article.html">
-                <img loading="lazy" decoding="async" src="images/post/post-2.jpg" alt="Post Thumbnail" class="w-100">
+              
+              @foreach ($recommended_posts as $post)
+              <a class="media align-items-center" href="{{$post->slug}}">
+                <img loading="lazy" decoding="async" src="/storage/{{$post->thumbnail->path}}" alt="Post Thumbnail" class="w-100">
                 <div class="media-body ml-3">
-                  <h3 style="margin-top:-5px">These Are Making It Easier To Visit</h3>
+                  <h3 style="margin-top:-5px">{{$post->title}}</h3>
                   <p class="mb-0 small">Heading Here is example of hedings. You can use …</p>
                 </div>
               </a>
-              <a class="media align-items-center" href="article.html"> <span class="image-fallback image-fallback-xs">No Image Specified</span>
-                <div class="media-body ml-3">
-                  <h3 style="margin-top:-5px">No Image specified</h3>
-                  <p class="mb-0 small">Lorem ipsum dolor sit amet, consectetur adipiscing …</p>
-                </div>
-              </a>
-              <a class="media align-items-center" href="article.html">
-                <img loading="lazy" decoding="async" src="/site-assets/images/post/post-5.jpg" alt="Post Thumbnail" class="w-100">
-                <div class="media-body ml-3">
-                  <h3 style="margin-top:-5px">Perfect For Fashion</h3>
-                  <p class="mb-0 small">Lorem ipsum dolor sit amet, consectetur adipiscing …</p>
-                </div>
-              </a>
-              <a class="media align-items-center" href="article.html">
-                <img loading="lazy" decoding="async" src="/site-assets/images/post/post-9.jpg" alt="Post Thumbnail" class="w-100">
-                <div class="media-body ml-3">
-                  <h3 style="margin-top:-5px">Record Utra Smooth Video</h3>
-                  <p class="mb-0 small">Lorem ipsum dolor sit amet, consectetur adipiscing …</p>
-                </div>
-              </a>
+              @endforeach
+              
             </div>
           </div>
         </div>
       </div>
+      @endif
       <div class="col-lg-12 col-md-6">
         <div class="widget">
           <h2 class="section-title mb-3">Categories</h2>
           <div class="widget-body">
             <ul class="widget-list">
-              <li><a href="#!">computer<span class="ml-auto">(3)</span></a>
-              </li>
-              <li><a href="#!">cruises<span class="ml-auto">(2)</span></a>
-              </li>
-              <li><a href="#!">destination<span class="ml-auto">(1)</span></a>
-              </li>
-              <li><a href="#!">internet<span class="ml-auto">(4)</span></a>
-              </li>
-              <li><a href="#!">lifestyle<span class="ml-auto">(2)</span></a>
-              </li>
-              <li><a href="#!">news<span class="ml-auto">(5)</span></a>
-              </li>
-              <li><a href="#!">telephone<span class="ml-auto">(1)</span></a>
-              </li>
-              <li><a href="#!">tips<span class="ml-auto">(1)</span></a>
-              </li>
-              <li><a href="#!">travel<span class="ml-auto">(3)</span></a>
-              </li>
-              <li><a href="#!">website<span class="ml-auto">(4)</span></a>
-              </li>
-              <li><a href="#!">hugo<span class="ml-auto">(2)</span></a>
-              </li>
+             @foreach (\App\Models\Category::all() as $category)
+             <li><a href="#!">{{$category}}<span class="ml-auto">(3)</span></a>
+             </li>
+             @endforeach
+              
             </ul>
           </div>
         </div>
