e5054e21e9879dab22dc47000447335075bf6f47
Arlindo <arlindoarnaldocassuende@gmail.com>
2024-06-23 20:20:53 +0100

    Linkando artigos no footer

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -6,7 +6,7 @@
             <img loading="prelaod" decoding="async" class="img-fluid" src="/site-assets/images/logo-white.png" alt="Reporter Hugo">
           </a>
           <ul class="p-0 d-flex navbar-footer mb-0 list-unstyled">
-            <li class="nav-item my-0"> <a class="nav-link" href="about.html">Artigos</a></li>
+            <li class="nav-item my-0"> <a class="nav-link" href="{{route('articles')}}">Artigos</a></li>
             <li class="nav-item my-0"> <a class="nav-link" href="article.html">Elements</a></li>
             <li class="nav-item my-0"> <a class="nav-link" href="privacy-policy.html">Políticas e privacidade</a></li>
             <li class="nav-item my-0"> <a class="nav-link" href="terms-conditions.html">Termos Condições</a></li>
